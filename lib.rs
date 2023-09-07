beed743e3c667263631603a4b87694290ad9f6ce
Gavin <gavin@rohrer.org>
2023-09-06 19:28:06 -0500

    Nothing new - fixed in 1.72.0

--- lib.rs
+++ lib.rs
@@ -7,11 +7,17 @@ where
     F: Fn(&T, &T) -> (),
 { }
 
+fn my_fn2<T>(a: T, b: T) {
+    a.partial_cmp(b)
+} 
+
+// note: rustc 1.71.0 (8ede3aae2 2023-07-12) running on x86_64-unknown-linux-gnu
 fn rustc_panic() {
+
+    MyStruct.partial_cmp(MyStruct);
 
     // the unit struct does not work
     my_fn::<_, MyStruct>( |a, b| {
-        a.partial_cmp(b)
+        a.partial_cmp(b);
     });
-
 }
